dfa3710b00fbbff60dfb1b72538d5910151f4c06
Mary Paz Ahumada Puello <mary.ahumada@udea.edu.co>
2026-03-22 13:35:37 -0500

    Creación de una función que selecciona aleatoriamente una pieza (I, O, T, S, Z)

---
+++ escogerPieza.cpp
@@ -0,0 +1,39 @@
+#include "escogerPieza.h"
+#include <cstdlib> // rand
+using namespace std;
+
+uint8_t* escogerPieza(short &altoPieza){
+
+    int tipo = rand() % 5;
+
+    uint8_t* pieza = nullptr;
+
+    switch(tipo){
+    case 0: // I
+        altoPieza = 4;
+        pieza = new uint8_t[4]{0b00010000,0b00010000,0b00010000,0b00010000};
+        break;
+
+    case 1: // O
+        altoPieza = 2;
+        pieza = new uint8_t[2]{0b00110000,0b00110000};
+        break;
+
+    case 2: // T
+        altoPieza = 3;
+        pieza = new uint8_t[3]{0b00111000,0b00010000,0b00000000};
+        break;
+
+    case 3: // S
+        altoPieza = 3;
+        pieza = new uint8_t[3]{0b00011000,0b00110000,0b00000000};
+        break;
+
+    case 4: // Z
+        altoPieza = 3;
+        pieza = new uint8_t[3]{0b00110000,0b00011000,0b00000000};
+        break;
+    }
+
+    return pieza;
+}
