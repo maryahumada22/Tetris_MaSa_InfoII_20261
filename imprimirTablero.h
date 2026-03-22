1df936bf5374953ee330dad8903e8ad47bf8ab6a
Mary Paz Ahumada Puello <mary.ahumada@udea.edu.co>
2026-03-22 13:42:02 -0500

    Creación de una función que integra la pieza seleccionada en el tablero usando máscaras de bits

---
+++ imprimirTablero.h
@@ -0,0 +1,9 @@
+#ifndef IMPRIMIRTABLERO_H
+#define IMPRIMIRTABLERO_H
+#include <cstdint>
+
+void imprimirTablero(uint8_t** matriz, uint8_t* pieza,
+            unsigned short int filas,
+            unsigned short int anchoByte);
+
+#endif // IMPRIMIRTABLERO_H
