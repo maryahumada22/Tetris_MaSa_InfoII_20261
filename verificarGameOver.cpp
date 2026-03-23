2076bdb1f06723d89ae28372ad973245a45339f0
Mary Paz Ahumada Puello <mary.ahumada@udea.edu.co>
2026-03-23 00:59:43 -0500

    Se simplifica el código

--- verificarGameOver.cpp
+++ verificarGameOver.cpp
@@ -1,13 +1,7 @@
 #include "verificarGameOver.h"
 
-bool verificarGameOver(uint8_t** tablero, unsigned short int anchoByte){
-
-    // revisa primera fila
-    for(int j = 0; j < anchoByte; j++){
-        if(tablero[0][j] != 0){
-            return true;
-        }
-    }
-
+bool verificarGameOver(uint8_t** tablero, unsigned short ancho){
+    for(int j=0;j<ancho;j++)
+        if(tablero[0][j]!=0) return true;
     return false;
 }
