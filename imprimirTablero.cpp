de00fc4ef5abd1a3d269cedea9f88eea17acca7d
Mary Paz Ahumada Puello <mary.ahumada@udea.edu.co>
2026-03-23 00:33:46 -0500

    Se establecen parámetros sobre la posición y la altura de la pieza (px, py, altoPieza). Se simplifica la lógica del código

--- imprimirTablero.cpp
+++ imprimirTablero.cpp
@@ -1,49 +1,27 @@
+#include "imprimirTablero.h"
 #include <iostream>
-#include <cstdint>
 using namespace std;
 
-void imprimirTablero(uint8_t** tablero, uint8_t* pieza,
-            unsigned short int filas,
-            unsigned short int anchoByte){
+void imprimirTablero(uint8_t** tablero,
+                     uint8_t* pieza,
+                     unsigned short filas,
+                     unsigned short ancho,
+                     uint8_t altoPieza,
+                     int px, int py){
 
-    //coordenadas de la pieza
+    for(int i=0;i<filas;i++){
+        for(int j=0;j<ancho;j++){
+            for(int bit=7;bit>=0;bit--){
 
+                uint8_t val = (tablero[i][j]>>bit)&1;
 
-    for (short int i=0; i<filas; i++) {
-        for (short int j=0; j<anchoByte; j++) {
-            //con esto mostraremos la pieza sin afectar al tablero
-            short pyInicial = 3;//altura de la pieza
-            short pxInicial = anchoByte/2; //un mal centrado por ahora.
-            //pieza -> tablero[pyInicial][pxInicial]
-            if (pxInicial==j && pyInicial>i){
-                //en este caso el sgte for se ejecuta 3 veces
-                for (short int bit=7; bit>=0;bit--) {
-                    uint8_t printP =
-                        ((pieza[i]>> bit) | (tablero[i][j]>> bit)) & 0b001;
-                    if (printP == 1){
-                        cout << "O";
-                    }
-                    else{
-                        cout << ".";
-                    }
+                if(j==px && i>=py && i<py+altoPieza){
+                    val |= (pieza[i-py]>>bit)&1;
                 }
-            }else{
-                for (short int bit=7; bit>=0;bit--) { //vamos de izquierda a derecha
-                    //tablero [filas][byte]
-                    uint8_t valor = (tablero[i][j] >> bit) & 0b001;
-                    //leemos y operamos sin modificar el tablero
 
-
-                    if (valor == 1){
-                        cout << "O";
-                    }
-                    else{
-                        cout << ".";
-                    }
-                }
+                cout<<(val?'O':'.');
             }
         }
-        cout << endl;
+        cout<<endl;
     }
 }
-
