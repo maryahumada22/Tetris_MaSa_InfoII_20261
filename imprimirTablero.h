ec5689e16b2a7c11524643653a9dacc9fd4dcabe
Mary Paz Ahumada Puello <mary.ahumada@udea.edu.co>
2026-03-23 00:36:01 -0500

    Se establecen parámetros sobre la posición y la altura de la pieza (px, py, altoPieza). Se simplifica la lógica del código.

--- imprimirTablero.h
+++ imprimirTablero.h
@@ -1,9 +1,13 @@
 #ifndef IMPRIMIRTABLERO_H
 #define IMPRIMIRTABLERO_H
+
 #include <cstdint>
 
-void imprimirTablero(uint8_t** matriz, uint8_t* pieza,
-            unsigned short int filas,
-            unsigned short int anchoByte);
+void imprimirTablero(uint8_t** tablero,
+                     uint8_t* pieza,
+                     unsigned short filas,
+                     unsigned short ancho,
+                     uint8_t altoPieza,
+                     int px, int py);
 
-#endif // IMPRIMIRTABLERO_H
+#endif
