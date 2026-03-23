4504b1e2f2a199e7f2dbeaa82b0c80855ef17160
Mary Paz Ahumada Puello <mary.ahumada@udea.edu.co>
2026-03-23 00:51:42 -0500

    Se añade la posición (px, py) las dimensiones, las colisiones, las funciones de movimiento, rotación y caída

--- pedirMovimiento.h
+++ pedirMovimiento.h
@@ -1,7 +1,13 @@
 #ifndef PEDIRMOVIMIENTO_H
 #define PEDIRMOVIMIENTO_H
+
 #include <cstdint>
 
-void pedirMovimiento(uint8_t** matriz, uint8_t* pieza);
+void pedirMovimiento(uint8_t** tablero,
+                     uint8_t* pieza,
+                     uint8_t &altoPieza,
+                     int &px, int &py,
+                     unsigned short filas,
+                     unsigned short ancho);
 
-#endif // PEDIRMOVIMIENTO_H
+#endif
